b2250d29227630f7e02b9b28586d521ce395d1ea
jparis <jparis@irishtimes.com>
2015-12-03 11:57:42 +0000

    Environment setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Todolists::Application.config.secret_key_base = 'd883780de74021d8185eda88b9d8b8722030426bd5bd9aa5e4b0e8c1433678aacb4ae93355f7be3a2e2ac7e835278549967c410cb170d308eb3416d06b14f044'
